78411eaeea0a60f4dd4f366613ad1110d7ed3326
yuihyama <yuihyama@gmail.com>
2018-10-29 17:19:37 +0900

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # math-py
+
+
+## License
+
+
+MIT License.
---
+++ sum/my_sum1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+"""my_sum1
+"""
+
+
+def my_sum1(n):
+    """
+
+    >>> my_sum1(10)
+    55.0
+    """
+    return (1/2)*n*(n + 1)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
