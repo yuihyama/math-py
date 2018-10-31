af38a1616fc08eab226e2ab5e787304e47423496
yuihyama <yuihyama@gmail.com>
2018-11-01 03:33:06 +0900

    Rename my_factorial() to my_factorial1()

--- factorial/my_factorial1.py
+++ factorial/my_factorial1.py
@@ -5,22 +5,22 @@
 """
 
 
-def my_factorial(n):
+def my_factorial1(n):
     """
 
-    >>> my_factorial(1)
+    >>> my_factorial1(1)
     1
-    >>> my_factorial(0)
+    >>> my_factorial1(0)
     1
-    >>> my_factorial(-1)
+    >>> my_factorial1(-1)
     1
-    >>> my_factorial(5)
+    >>> my_factorial1(5)
     120
     """
     if n < 2:
         return 1
     else:
-        return n * my_factorial(n - 1)
+        return n * my_factorial1(n - 1)
 
 
 if __name__ == '__main__':
